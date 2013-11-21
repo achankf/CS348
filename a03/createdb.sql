9ed508e6e3a45bf51a531a33c754025bb532e86d
Alfred Chan <a77chan@uwaterloo.ca>
2013-11-20 19:45:59 -0500

    created latex template for a03

--- a03/createdb.sql
+++ a03/createdb.sql
@@ -1,6 +1,4 @@
-
 connect to cs348
-
 drop table product
 create table product( \
 	manufacturer varchar(100) not null, \
@@ -27,7 +25,17 @@ create table purchase_order( \
 	model_number integer not null, \
 	cid integer not null references customer(cid), \
 	sold_price double not null, \
-	-- unique (manufacturer, model_number), \
+	primary key (cid, manufacturer, model_number), \
+	foreign key (manufacturer, model_number) references product(manufacturer, model_number) \
+)
+
+drop table evaluation
+create table evaluation( \
+	manufacturer varchar(100) not null, \
+	model_number integer not null, \
+	cid integer not null references customer(cid), \
+	score int not null, \
+	check (score >= 1 and score <= 5), \
 	primary key (cid, manufacturer, model_number), \
 	foreign key (manufacturer, model_number) references product(manufacturer, model_number) \
 )
@@ -53,10 +61,56 @@ create table camera( \
 	foreign key (manufacturer, model_number) references product(manufacturer, model_number) \
 )
 
+drop table camera_normal_feature
+create table camera_normal_feature( \
+	manufacturer varchar(100) not null, \
+	model_number integer not null, \
+	feature_number integer not null, \
+	check (feature_number = 2), -- ONLY #2 "FOR NOW" \
+	primary key (manufacturer, model_number, feature_number), \
+	foreign key (manufacturer, model_number) references camera(manufacturer, model_number) \
+) 
+
+drop table camera_distinct_feature
+create table camera_distinct_feature( \
+	manufacturer varchar(100) not null, \
+	model_number integer not null, \
+	feature_number integer not null, \
+	check (feature_number >= 3 and feature_number <= 5), -- one of {3,4,5} \
+	primary key (manufacturer, model_number), -- ONE SUCH FEATURE PER CAMERA \
+	foreign key (manufacturer, model_number) references camera(manufacturer, model_number) \
+) 
+
+drop table lens_replaceable_camera
 create table lens_replaceable_camera( \
 	manufacturer varchar(100) not null, \
 	model_number integer not null, \
-	
+	primary key (manufacturer, model_number), \
+	foreign key (manufacturer, model_number) references camera(manufacturer, model_number) \
+)
+
+drop table lens_repl_cam_has_lens
+create table lens_repl_cam_has_lens( \
+	manufacturer varchar(100) not null, \
+	model_number integer not null, \
+	lens_manufacturer varchar(100) not null, \
+	lens_model_number integer not null, \
+	primary key (manufacturer, model_number, lens_manufacturer, lens_model_number), \
+	foreign key (manufacturer, model_number) references lens_replaceable_camera(manufacturer, model_number) \
+)
+
+drop table lens_non_replaceable_camera
+create table lens_non_replaceable_camera( \
+	manufacturer varchar(100) not null, \
+	model_number integer not null, \
+	lens_manufacturer varchar(100) not null, -- ONLY HAVE ONE LENS \
+	lens_model_number integer not null, \
+	aperture_range double not null, \
+	min_focal_range double not null, \
+	max_focal_range double not null, \
+	primary key (manufacturer, model_number), \
+	foreign key (manufacturer, model_number) references camera(manufacturer, model_number), \
+	foreign key (lens_manufacturer, lens_model_number) references camera(manufacturer, model_number) \
+)
 
 insert into customer values (0, 'Alfred', 'a77chan@uwaterloo.ca', 'abc street', 'domestic')
-insert into customer values (1, 'Alfred', 'a77chan@uwaterloo.ca', 'dba street', 'dome')
