2ff4cbfbb5bfeb38805d85dd3073a26e55f2bb42
Alfred Chan <a77chan@uwaterloo.ca>
2013-11-22 14:15:45 -0500

    simplified query

--- a03/createdb.sql
+++ a03/createdb.sql
@@ -1,12 +1,11 @@
 connect to cs348
 drop table product
 create table product( \
+	model_number integer not null primary key, \
 	manufacturer varchar(100) not null, \
-	model_number integer not null, \
 	release_date date not null, \
 	retail_price double not null, \
-	num_stock integer not null, \
-	primary key (manufacturer, model_number) \
+	num_stock integer not null \
 )
 
 drop table customer 
@@ -21,95 +20,59 @@ create table customer( \
 
 drop table purchase_order
 create table purchase_order( \
-	manufacturer varchar(100) not null, \
-	model_number integer not null, \
+	model_number integer not null references product, \
 	cid integer not null references customer(cid), \
 	sold_price double not null, \
-	primary key (cid, manufacturer, model_number), \
-	foreign key (manufacturer, model_number) references product(manufacturer, model_number) \
-)
-
-drop table evaluation
-create table evaluation( \
-	manufacturer varchar(100) not null, \
-	model_number integer not null, \
-	cid integer not null references customer(cid), \
 	score int not null, \
-	check (score >= 1 and score <= 5), \
-	primary key (cid, manufacturer, model_number), \
-	foreign key (manufacturer, model_number) references product(manufacturer, model_number) \
+	check (score >= 1 and score <= 5) \
 )
 
 drop table lens
 create table lens( \
-	manufacturer varchar(100) not null, \
-	model_number integer not null, \
+	lens_model_number integer not null primary key references product, \
 	aperture_range double not null, \
 	max_focal_length double not null, \
-	min_focal_length double not null, \
-	primary key (manufacturer, model_number), \
-	foreign key (manufacturer, model_number) references product(manufacturer, model_number) \
+	min_focal_length double not null \
 )
 
 drop table camera
 create table camera( \
-	cam_manufacturer varchar(100) not null, \
-	cam_model_number integer not null, \
+	cam_model_number integer not null primary key references product, \
 	sensor_size double not null, \
-	pixel_number integer not null, \
-	primary key (cam_manufacturer, cam_model_number), \
-	foreign key (cam_manufacturer, cam_model_number) references product(manufacturer, model_number) \
+	pixel_number integer not null \
 )
 
 drop table camera_normal_feature
 create table camera_normal_feature( \
-	cam_manufacturer varchar(100) not null, \
-	cam_model_number integer not null, \
+	cam_model_number integer not null primary key references camera, \
 	feature_number integer not null, \
-	check (feature_number = 2), -- ONLY #2 "FOR NOW" \
-	primary key (cam_manufacturer, cam_model_number, feature_number), \
-	foreign key (cam_manufacturer, cam_model_number) references camera(manufacturer, model_number) \
+	check (feature_number = 2) -- ONLY #2 "FOR NOW" \
 ) 
 
 drop table camera_distinct_feature
 create table camera_distinct_feature( \
-	cam_manufacturer varchar(100) not null, \
-	cam_model_number integer not null, \
+	cam_model_number integer not null primary key references camera, \
 	feature_number integer not null, \
-	check (feature_number >= 3 and feature_number <= 5), -- one of {3,4,5} \
-	primary key (cam_manufacturer, cam_model_number), -- ONE SUCH FEATURE PER CAMERA \
-	foreign key (cam_manufacturer, cam_model_number) references camera(manufacturer, model_number) \
+	check (feature_number >= 3 and feature_number <= 5) -- one of {3,4,5} \
 ) 
 
 drop table lens_replaceable_camera
 create table lens_replaceable_camera( \
-	cam_manufacturer varchar(100) not null, \
-	cam_model_number integer not null, \
-	primary key (cam_manufacturer, cam_model_number), \
-	foreign key (cam_manufacturer, cam_model_number) references camera(manufacturer, model_number) \
+	cam_model_number integer not null primary key references camera \
 )
 
 drop table lens_repl_cam_has_lens
 create table lens_repl_cam_has_lens( \
-	cam_manufacturer varchar(100) not null, \
-	cam_model_number integer not null, \
-	lens_manufacturer varchar(100) not null, \
-	lens_model_number integer not null, \
-	primary key (cam_manufacturer, cam_model_number, lens_manufacturer, lens_model_number), \
-	foreign key (lens_manufacturer, lens_model_number) references lens(manufacturer, model_number) \
-	foreign key (cam_manufacturer, cam_model_number) references lens_replaceable_camera(manufacturer, model_number) \
+	cam_model_number integer not null references camera, \
+	lens_model_number integer not null references lens, \
+	primary key (cam_model_number, lens_model_number) \
 )
 
 drop table lens_non_replaceable_camera
 create table lens_non_replaceable_camera( \
-	cam_manufacturer varchar(100) not null, \
-	cam_model_number integer not null, \
-	lens_manufacturer varchar(100) not null, -- ONLY HAVE ONE LENS \
-	lens_model_number integer not null, \
+	cam_model_number integer not null primary key references camera, \
+	lens_model_number integer not null references lens, \
 	aperture_range double not null, \
 	min_focal_range double not null, \
-	max_focal_range double not null, \
-	primary key (cam_manufacturer, cam_model_number), \
-	foreign key (cam_manufacturer, cam_model_number) references camera(manufacturer, model_number), \
-	foreign key (lens_manufacturer, lens_model_number) references lens(manufacturer, model_number) \
+	max_focal_range double not null \
 )
